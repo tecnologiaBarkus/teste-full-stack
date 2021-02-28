e17def8ca1e37cf95404ce2a218459cea5c5cabd
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2021-02-28 19:23:49 +0000

    conecta com banco de dados

--- challengePokemon/dist/app.js
+++ challengePokemon/dist/app.js
@@ -6,7 +6,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 var express_1 = __importDefault(require("express"));
 var routes_1 = require("./routes");
+var mongoose_1 = __importDefault(require("mongoose"));
+var dotenv_1 = require("dotenv");
+dotenv_1.config();
 var app = express_1.default();
 exports.app = app;
 app.use(express_1.default.json());
 app.use(routes_1.router);
+mongoose_1.default.connect("mongodb+srv://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASS +
+    ("@" + process.env.MONGO_CLUSTER + "/" + process.env.MONGO_DB + "?retryWrites=true&w=majority"), {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (err) {
+    if (!err) {
+        console.log('connected to database!');
+    }
+    else {
+        console.log('mongo error', err);
+        console.log('user', process.env.MONGO_USER);
+    }
+});
--- challengePokemon/src/app.ts
+++ challengePokemon/src/app.ts
@@ -1,9 +1,27 @@
 import express from 'express'
 import { router } from "./routes"
+import mongoose from "mongoose"
+import { config } from "dotenv";
+
+config()
 
 const app  = express()
 
 app.use(express.json())
 app.use(router)
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}` + 
+`@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if(!err) {
+        console.log('connected to database!')
+    } else {
+        console.log('mongo error', err)
+        console.log('user', process.env.MONGO_USER)
+    }
+})
 
 export { app }
